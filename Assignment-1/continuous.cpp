5ce38ba2da08936c769b233c573f7a749edd0b67
KozlowskiMatt <kozlowsm@mcmaster.ca>
2022-09-25 14:18:38 -0400

    Updated continuous

--- Assignment-1/continuous.cpp
+++ Assignment-1/continuous.cpp
@@ -2,27 +2,17 @@
 #include <fstream>
 #include <eigen3/Eigen/Dense>
 #include <cmath>
-
 using namespace std;
 
-// Continuous representation using Runge-kutta technique
+/*
+    Continuous simuilation of a projectile using Runge-kutta technique
+*/
 
-double g = -9.8;
-
-Eigen::VectorXd u_func(double t,const Eigen::VectorXd &in)
-{
-
-	Eigen::VectorXd out;
-
-	return(out);
-}
-
-Eigen::VectorXd m_func(double t,const Eigen::VectorXd &in)
+Eigen::VectorXd calculate(double t,const Eigen::VectorXd &in)
 {
 	Eigen::VectorXd out;
+    const double g = -9.8; //Acceleration due to gravity
 
-
-//	Eigen::VectorXd u(1);
 	Eigen::MatrixXd A(4, 4);
     Eigen::MatrixXd k(4, 1);
 	A << 0,1,0,0,
@@ -32,51 +22,50 @@ Eigen::VectorXd m_func(double t,const Eigen::VectorXd &in)
     
     k << 0,0,0,g;
 
-	out= A*in + k;
+	out= A*in + k; // Implementation of state space equation X'(t) = A*x(t) +Bu(t) + k || B = 0
 	return(out);
 }
 
-Eigen::VectorXd run(double t, const Eigen::VectorXd &in, double h)
+// Implemenation of the Runge-Kutta technique
+Eigen::VectorXd Runge(double t, const Eigen::VectorXd &in, double h)
 {
         Eigen::VectorXd k1, k2, k3, k4;
-        k1 = m_func(t, in);
-        k2 = m_func(t + h / 2, in + (h / 2) * k1);
-        k3 = m_func(t + h / 2, in + (h / 2) * k2);
-        k4 = m_func(t + h, in + h * k3);
+        k1 = calculate(t, in);
+        k2 = calculate(t + h / 2, in + (h / 2) * k1);
+        k3 = calculate(t + h / 2, in + (h / 2) * k2);
+        k4 = calculate(t + h, in + h * k3);
 
-        return in + h* (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+        return (in + h* (k1 + 2 * k2 + 2 * k3 + k4) / 6);
 }
 
-void solve(const Eigen::VectorXd &x0,double h,double time)
+void solver(const Eigen::VectorXd &x0,double h,double time)
 {
 	ofstream myfile;
 	myfile.open ("Continuous_Data");
 
-	double  t;
+	double  t; // Keep track of time in while loop
 	t=0;
 	Eigen::VectorXd x=x0;
     myfile<<"\t"<<t<<"\t"<<x(0,0)<<"\t"<<x(1,0)<<"\t"<<x(2,0)<<"\t"<<x(3,0)<<endl;
     t=t+h;
 	while(t<time){
-		x=run(t,x,h);
-        myfile<<"\t"<<t<<"\t"<<x(0,0)<<"\t"<<x(1,0)<<"\t"<<x(2,0)<<"\t"<<x(3,0)<<endl;
+		x=Runge(t,x,h);
+        myfile<<"\t"<<t<<"\t"<<x(0,0)<<"\t"<<x(1,0)<<"\t"<<x(2,0)<<"\t"<<x(3,0)<<endl; //Formatting 
 		t=t+h;
 	}
 }
 
 int main(void)
 {
-        
 	Eigen::VectorXd X(4,1);
-    Eigen::VectorXd Y(4,1);
     double h, velocity, total_time, t,alpha;
-    velocity = 20;
-    alpha = 60;
+    velocity = 20; // Initial velocity = 20 m/s
+    alpha = 60; // Launch angle = 60 deg
     h = 0.01;
-    total_time = (velocity*sin(alpha*M_PI/180))/4.9;
-    
+    total_time = (velocity*sin(alpha*M_PI/180))/4.9; // Time projectile is in air
 
-	X << 0,velocity*cos(alpha*M_PI/180),0,velocity*sin(alpha*M_PI/180);
-    solve(X,h,total_time);
+	X << 0,velocity*cos(alpha*M_PI/180),0,velocity*sin(alpha*M_PI/180); //Initial vector [pos_x,vel_x,pos_y,vel_y]
+    solver(X,h,total_time);
+    
     return 0;
 }
